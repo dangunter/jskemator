4cbe36fc23d7ed8de49ee891f76394871b3f0253
Dan Gunter <dkgunter@lbl.gov>
2012-05-17 06:34:33 -0700

    streamlining

--- jskemator.py
+++ jskemator.py
@@ -12,100 +12,92 @@ pp = pprint.PrettyPrinter(indent=4)
 
 _SCALARS = set((str, int, long, float))
 
+def is_scalar(x):
+    return type(x) in _SCALARS
+    
 def _scalars(x):
     """Return True if the list `x` is only composed
     of scalars, otherwise false. 
     """
-    return all(map(lambda y: type(y) in _SCALARS, x))
+    return all(map(is_scalar, x))
 
-class Skema:
-    def __init__(self, json, schema=None, simple_lists=False):
+class Schema(dict):
+    # If True, do not generate schema items
+    # for each value in a list of scalars.
+    simple_lists = False
+
+    def __init__(self, json, schema={}):
         """Create new schema skeleton.
         
         Args:
             - `json` = JSON object, a dictionary.
             - `schema` = Optional schema, a dictionary
-            - `simple_lists` = If True, do not generate schema items
-                               for each value in a list of scalars.
         """
-        self.obj, self.s = json, schema
-        self._skip_list_scalars = not simple_lists
+        obj = self._skemate(json, schema)
+        dict.__init__(self, obj)
 
-    def set_defaults (self, s):
-        default = { }
-        if s != None:
-            default['description'] = s.get('description','None')
-            default['additionalProperties'] = (
-                s.get('additionalProperties', False))
-            default['required'] = s.get('required', True)
-        else:
-            default['description'] = 'Dummy description'
-            default['additionalProperties'] = False
-            default['required'] = True
-        return default
-
-    def skemate(self):
-        return self._skemate(self.obj, self.s)
-
-    def _skemate(self, o, s=None):
+    def _skemate(self, obj, old_schema={}):
         """Dispatch to appropriate processing function for the
-        type of the object `o`, pulling default values from the
-        existing schema `s`.
+        type of the object `obj`, pulling default values from the
+        existing schema `old_schema`.
         """
-        typestr = '_' + o.__class__.__name__
+        typestr = '_' + obj.__class__.__name__
         if not hasattr(self, typestr):
-            raise ValueError("Unrecognized type for {}".format(o))
-        return getattr(self, typestr)(o, s)
+            raise ValueError("Unrecognized type for {}".format(typestr))
+        # set defaults
+        schema = { 'description' : "",
+                   'additionalProperties' : False,
+                   'required' : True }
+        # copy values from old_schema
+        for k in schema.keys():
+            if k in old_schema:
+                schema[k] = old_schema[k]
+        # add properties for obj
+        schema.update(getattr(self, typestr)(obj, old_schema))
+        return schema
 
     def _dict(self, d, s):
         "Process a dict."
-        skema = self.set_defaults(s)
-        skema['type'] = 'object'
-        skema['properties'] = { }
-        for key, value in d.items ():
-            if s is None:
-                new_s = None
-            else:
-                # match properties in schema, but default to None
-                new_s = s['properties'].get(key, None)
-            skema['properties'][key] = self._skemate(value, new_s)
+        skema = { 'type' : 'object', 'properties' : { } }
+        sprops = s.get('properties', { })
+        for key, value in d.items():
+            old_props = sprops.get(key, { })
+            skema['properties'][key] = self._skemate(value, old_props)
         return skema
 
-    def _list(self, l, s):
+    def _list(self, items, s):
         """Process a list.
         
         If `simple_lists` was False in the constructor, then
         do not recurse into lists that have only scalar values.
         """
-        skema=self.set_defaults(s)
         skema['type'] = 'array'
-        if self._skip_list_scalars and _scalars(l):
-            return skema
+        if not self.simple_lists and _scalars(items):
+            return skema # no properties
         skema['properties'] = [ ]
-        for value in l:
-            skema['properties'].append(self._skemate(value)) 
+        for v in items:
+            skema['properties'].append(self._skemate(v)) 
         return skema
 
     _tuple = _list
     
     def _str(self, v, s):
         "Process a string."
-        return _scalar("string", v, s)
+        return self._scalar("string", v, s)
 
     def _int(self, v, s):
         "Process an integer"
-        return _scalar("integer", v, s)
+        return self._scalar("integer", v, s)
 
     def _float(self, v, s):
         "Process a float"
-        return _scalar("float", v, s)
+        return self._scalar("float", v, s)
         
     def _scalar(self, typename, val, schema):
-        res = self.set_defaults(schema)
-        res['type'] = typename
-        res['pattern'] = ''
-        res['value'] = val
-        return res
+        return {'type' : typename,
+                'pattern' : '',
+                'value' : val # XXX: is this really useful?
+        }
 
 def main(argv):
     """Process command-line options and run.
@@ -147,9 +139,8 @@ def main(argv):
             parser.error("Could not decode schema file {f}".format(f=sfile))
     else:
         schema_obj = None
-    jskemator = JSkemator(json=json_obj, schema=schema_obj)
-    skema = jskemator.skemate()
-    print(json.dumps(skema, indent=4))
+    sk = Schema(json=json_obj, schema=schema_obj)
+    print(json.dumps(sk, indent=4))
     return 0
     
 if __name__ == '__main__':
